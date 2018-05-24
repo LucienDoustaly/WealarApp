a7d3f6fdd29dd6faf805350dc1063510ee5a01db
Unknown <luciendoustaly@icloud.com>
2018-05-24 11:57:46 +0200

    Update imgs sliders

--- src/pages/firstconnection/firstconnection.ts
+++ src/pages/firstconnection/firstconnection.ts
@@ -26,7 +26,7 @@ export class FirstconnectionPage {
     {
       title: "What is Wealar?",
       description: "<b>Wealar</b> is a remote connected alarm with a weather station which can allow you to get information near your home",
-      image: "assets/img/logo_transparent.png",
+      image: "assets/imgs/logo_transparent.png",
     },
     {
       title: "What is Wealar app?",
