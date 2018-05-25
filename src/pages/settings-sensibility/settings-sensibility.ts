6e9c8f0f4daea0a1214bc27043315f423cfc33e8
Unknown <luciendoustaly@icloud.com>
2018-05-25 03:30:09 +0200

    Description of settings

--- src/pages/settings-sensibility/settings-sensibility.ts
+++ src/pages/settings-sensibility/settings-sensibility.ts
@@ -23,17 +23,17 @@ export class SettingsSensibilityPage {
   items: any = {
     'LOW': [
       {
-        message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        message: 'In LOW MODE presence detector sensibility is desactivated.'
       }
     ],
     'MEDIUM': [
       {
-        message: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+        message: 'In MEDIUM MODE presence detector sensibility is low.'
       }
     ],
     'HIGH': [
       {
-        message: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Phasellus ac massa gravida libero accumsan auctor et ut diam. Sed feugiat sed nibh at vulputate. Duis lobortis venenatis nisi id lobortis. Pellentesque at imperdiet risus. Nam vel lectus vel enim ullamcorper suscipit vestibulum at orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. '
+        message: 'In HIGH MODE presence detector sensibility is high.'
       }
     ]
   };
